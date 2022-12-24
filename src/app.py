e5931cc4d97283096e60f850f59116c1f58424c3
Elizabeth <correo@correo.com>
2022-12-23 21:34:28 -0300

    cambios conexion bd

--- src/app.py
+++ src/app.py
@@ -1,9 +1,27 @@
 from flask import Flask
-from flask import template_rendered
+from flask import render_template
 from flaskext.mysql import MySQL
 
 app=Flask (__name__)
 mysql=MySQL()
+
+app.config['MYSQL_DATABASE_HOST']='localhost'
+app.config['MYSQL_DATABASE_USER']='root'
+app.config['MYSQL_DATABASE_PASSWORD']='root'
+app.config['MYSQL_DATABASE_DB']='empleados'
+
+mysql.init_app(app)
+
+@app.route('/')
+def index();
+    conn = mysql.connect()
+    cursor = conn.cursor()
+
+    sql="insert into empleados (nombre, correo, foto) values ('Ely', 'ely@email.com', 'fotodeely.jpg');"
+    cursor.execute(sql)
+    conn.commit()
+
+    return render_template('empleados/index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
