aabd2cfdbba352b25f5a43c6cd92044fc49e1475
Elizabeth <correo@correo.com>
2022-12-26 02:13:51 -0300

    con edit y modify

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import render_template, request, redirect
 from flaskext.mysql import MySQL
 from datetime import datetime
+import os
 
 app=Flask (__name__)
 mysql=MySQL()
@@ -11,6 +12,8 @@ app.config['MYSQL_DATABASE_USER']='root'
 app.config['MYSQL_DATABASE_PASSWORD']='root'
 app.config['MYSQL_DATABASE_DB']='empleados'
 
+UPLOADS = os.path.join('uploads')
+app.config['UPLOADS'] = UPLOADS #guardamos la ruta con un valor en la apps
 mysql.init_app(app)
 
 @app.route('/')
@@ -53,6 +56,57 @@ def store():
     conn.commit()
 
     return redirect('/')
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    sql="DELETE FROM empleados WHERE id=%s"
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute(sql, id)
+    conn.commit()
+
+    return redirect('/')
+
+@app.route('/modify/<int:id>')
+def modify(id):
+    sql=f'SELECT * FROM empleados WHERE id={id}'
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    empleado=cursor.fetchone()
+    conn.commit()
+
+    return render_template('empleados/edit.html', empleado=empleado)
+
+@app.route('/update', methods=['POST'])
+def update():
+    _nombre = request.form['txtnombre']
+    _correo = request.form['txtcorreo']
+    _foto = request.form['txtFoto']
+    id = request.form['txtId']
+
+    datos=(_nombre, _correo, id)
+    conn = mysql.connect()
+    cursor = conn.cursor()
+
+    if _foto.filename != '':
+        now = datetime.now()
+        tiempo = now.strptime("%Y%H%M%S")
+        nuevoNombreFoto = tiempo + '_' + _foto.filename
+        _foto.save("uploads/" + nuevoNombreFoto)
+    
+    sql = f'SELECT foto FROM empleados WHERE id={id}'
+    cursor.execute(sql)
+    
+    nombreFoto=cursor.fetchone()[0]
+    os.remove(os.path.join(app.config['UPLOADS'], nombreFoto))
+
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    
+    sql = f'UPDATE empleados SET nombre={_nombre}, correo={_correo} WHERE id={id}'
+    cursor.execute(sql)
+    conn.cursor()
 
 if __name__ == '__main__':
     app.run(debug=True)
